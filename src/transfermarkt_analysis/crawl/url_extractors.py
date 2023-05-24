cd5ab007a784a015bf2c62ed70e650fcbe356090
ilia4bit <iliark82@gmail.com>
2023-05-24 21:19:29 +0330

    create - matchday_urls_extractor, store result in data/urls

--- src/transfermarkt_analysis/crawl/url_extractors.py
+++ src/transfermarkt_analysis/crawl/url_extractors.py
@@ -1,14 +1,19 @@
 import os
 
 import pandas as pd
+import requests
 import urllib3
 from bs4 import BeautifulSoup
 
 from transfermarkt_analysis.crawl.consts import (BASE_URL, HEADERS,
+                                                 LEAGUE_MATCHDAY_URLS,
                                                  LEAGUE_TRANSFERS_URLS,
-                                                 SEASONS_RANGE, URLS_DIR)
+                                                 MATHDAY_RANGE, SEASONS_RANGE,
+                                                 URLS_DIR)
 
 http = urllib3.PoolManager(headers=HEADERS)
+
+# extractor functions
 
 
 def player_urls_extractor():
@@ -43,6 +48,27 @@ def team_urls_extractor():
                 yield {"url": BASE_URL + result["href"]}
 
 
+def matchday_urls_extractor():
+    """
+    get url for each matchday in each season of each league
+    """
+    for league_name, league_url in LEAGUE_MATCHDAY_URLS.items():
+        for season in SEASONS_RANGE:
+            for matchday in MATHDAY_RANGE:
+                resp = http.request(
+                    "GET",
+                    league_url,
+                    fields={"saison_id": season, "spieltag": matchday},
+                )
+                soup = BeautifulSoup(resp.data, "html.parser")
+                selector = ".responsive-table > table:nth-child(1) > tbody:nth-child(2) > tr > td:nth-child(7) > span > a"
+                for result in soup.select(selector, href=True):
+                    yield {"url": BASE_URL + result["href"]}
+
+
+# store functions
+
+
 def store_player_urls():
     """
     call player_urls_extractor and store result as pandas.DataFrame
@@ -62,6 +88,14 @@ def store_team_urls():
     df.to_csv(URLS_DIR / "team_urls.csv", index=False)
 
 
+def store_matchday_urls():
+    """
+    call matchday_urls_extractor and store result as pandas.DataFrame
+    then use .to_csv method to store it as csv file in crawl/data/urls dir
+    """
+    df = pd.DataFrame(matchday_urls_extractor())
+    df.to_csv(URLS_DIR / "matchday_urls.csv", index=False)
+
 
 def store_all_urls():
     """
@@ -70,6 +104,7 @@ def store_all_urls():
     store_funcs = {
         "player_urls.csv": store_player_urls,
         "team_urls.csv": store_team_urls,
+        "matchday_urls.csv": store_matchday_urls,
     }
 
     for csv_file in store_funcs.keys():
@@ -77,3 +112,6 @@ def store_all_urls():
             print(f"already {csv_file} exists in {URLS_DIR}")
         else:
             store_funcs[csv_file]()
+
+
+store_all_urls()
