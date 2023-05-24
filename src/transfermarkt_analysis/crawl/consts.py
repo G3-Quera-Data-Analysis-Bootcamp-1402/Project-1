03000a59115322a1f4c34e8ca87fb3e9f10b332e
ilia4bit <iliark82@gmail.com>
2023-05-24 17:36:01 +0330

    update - add LEAGUE_MATCHDAY_URLS

--- src/transfermarkt_analysis/crawl/consts.py
+++ src/transfermarkt_analysis/crawl/consts.py
@@ -10,6 +10,14 @@ LEAGUE_TRANSFERS_URLS = {
     "france": "https://transfermarkt.com/ligue-1/transfers/wettbewerb/FR1",
 }
 
+LEAGUE_MATCHDAY_URLS = {
+    "england": "https://www.transfermarkt.com/premier-league/spieltagtabelle/wettbewerb/GB1/",
+    "spain": "https://transfermarkt.com/laliga/spieltagtabelle/wettbewerb/ES1",
+    "germany": "https://transfermarkt.com/bundesliga/spieltagtabelle/wettbewerb/L1",
+    "italy": "https://transfermarkt.com/serie-a/spieltagtabelle/wettbewerb/IT1",
+    "france": "https://transfermarkt.com/ligue-1/spieltagtabelle/wettbewerb/FR1",
+}
+
 HEADERS = {
     "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36"
 }
