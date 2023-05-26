c613ba97e8afba0571ec793e4ea1b3b5ab96b80a
ilia4bit <iliark82@gmail.com>
2023-05-26 17:44:07 +0330

    change .com to .de domain

--- src/transfermarkt_analysis/crawl/consts.py
+++ src/transfermarkt_analysis/crawl/consts.py
@@ -1,21 +1,21 @@
 import pathlib
 
-BASE_URL = "https://transfermarkt.com"
+BASE_URL = "https://transfermarkt.de"
 
 LEAGUE_TRANSFERS_URLS = {
-    "england": "https://transfermarkt.com/premier-league/transfers/wettbewerb/GB1",
-    "spain": "https://transfermarkt.com/laliga/transfers/wettbewerb/ES1",
-    "germany": "https://transfermarkt.com/bundesliga/transfers/wettbewerb/L1",
-    "italy": "https://transfermarkt.com/serie-a/transfers/wettbewerb/IT1",
-    "france": "https://transfermarkt.com/ligue-1/transfers/wettbewerb/FR1",
+    "england": "https://transfermarkt.de/premier-league/transfers/wettbewerb/GB1",
+    "spain": "https://transfermarkt.de/laliga/transfers/wettbewerb/ES1",
+    "germany": "https://transfermarkt.de/bundesliga/transfers/wettbewerb/L1",
+    "italy": "https://transfermarkt.de/serie-a/transfers/wettbewerb/IT1",
+    "france": "https://transfermarkt.de/ligue-1/transfers/wettbewerb/FR1",
 }
 
 LEAGUE_MATCHDAY_URLS = {
-    "england": "https://www.transfermarkt.com/premier-league/spieltagtabelle/wettbewerb/GB1/",
-    "spain": "https://transfermarkt.com/laliga/spieltagtabelle/wettbewerb/ES1",
-    "germany": "https://transfermarkt.com/bundesliga/spieltagtabelle/wettbewerb/L1",
-    "italy": "https://transfermarkt.com/serie-a/spieltagtabelle/wettbewerb/IT1",
-    "france": "https://transfermarkt.com/ligue-1/spieltagtabelle/wettbewerb/FR1",
+    "england": "https://www.transfermarkt.de/premier-league/spieltagtabelle/wettbewerb/GB1/",
+    "spain": "https://transfermarkt.de/laliga/spieltagtabelle/wettbewerb/ES1",
+    "germany": "https://transfermarkt.de/bundesliga/spieltagtabelle/wettbewerb/L1",
+    "italy": "https://transfermarkt.de/serie-a/spieltagtabelle/wettbewerb/IT1",
+    "france": "https://transfermarkt.de/ligue-1/spieltagtabelle/wettbewerb/FR1",
 }
 
 HEADERS = {
