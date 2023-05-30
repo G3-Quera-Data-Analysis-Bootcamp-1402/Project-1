87f3ec589d4861629552e7c9dc70c0baad34446b
ilia4bit <iliark82@gmail.com>
2023-05-30 17:28:00 +0330

    update scripts

--- scripts/db_setup.py
+++ scripts/db_setup.py
@@ -1,4 +1,10 @@
+from colorama import Fore
+
 from transfermarkt_analysis.db.schema import create_tables
 
 
-create_tables()
+try:
+    create_tables()
+    print(Fore.GREEN + "Database successfully created :)")
+except Exception as err:
+    raise err
