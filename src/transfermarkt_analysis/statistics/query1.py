8856f323b3d7f664fbee5e802e21ca3c8efc445c
mtdamir <amir.motavas79@gmail.com>
2023-05-31 08:48:23 +0330

    finall

--- src/transfermarkt_analysis/statistics/query1.py
+++ src/transfermarkt_analysis/statistics/query1.py
@@ -7,18 +7,29 @@ def games_played():
     matchs_df = pd.read_csv(CLEANIZED_DIR / "matches.csv").reset_index(drop= True)
     concat_df = pd.concat([df,matchs_df], axis=1)
     result = concat_df.loc[concat_df['season_id'] == 2021 ].groupby('player_id').count().reset_index().loc[:,['player_id','season_id']]
-    return result['season_id']
+    match_count = result['season_id']
+    games_played_distribution = match_count.value_counts(normalize=True)
+    total_games = match_count.sum()
+    player_games_percentage = (match_count / total_games) * 100
+    print(games_played_distribution)
+    print(player_games_percentage)
 
 
 
-# Calculate the distribution of the number of games played by players
-match_count = games_played()
-games_played_distribution = match_count.value_counts(normalize=True)
 
 
-# Calculate the percentage of games that players participate in
-total_games = match_count.sum()
-player_games_percentage = (match_count / total_games) * 100
 
-print(games_played_distribution)
-print(player_games_percentage)
+
+
+
+def goal_market_value():
+    goal_df = pd.read_csv(CLEANIZED_DIR / "goals.csv")
+    mv_df= pd.read_csv(CLEANIZED_DIR / "market_values.csv")
+    matchs_df = pd.read_csv(CLEANIZED_DIR / "matches.csv").reset_index(drop= True)
+    concat_df = pd.concat([goal_df,mv_df,matchs_df], axis=1)
+
+
+    x = concat_df.loc[concat_df['season_id'] == 2021].groupby('scorrer_id').count().loc[:,'match_id']
+    print(x)
+
+goal_market_value
